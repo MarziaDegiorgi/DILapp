0866b357660759f192d3a35f2e8c13056fcb4b7a
Giulia Leonardi <giulia.leonardi@mail.polimi.it>
2017-11-20 16:24:09 +0100

    Intro

--- app/src/main/java/com/polimi/dilapp/MainActivity.java
+++ app/src/main/java/com/polimi/dilapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.polimi.dilapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView introVideoView = (VideoView) findViewById(R.id.intro);
+        introVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro));
+        introVideoView.start();
     }
 }
