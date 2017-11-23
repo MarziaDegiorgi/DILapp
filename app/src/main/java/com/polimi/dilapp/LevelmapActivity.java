c6fcd8e7abc9221048c567642e3a745f3618040b
marziadegiorgi <marziadegio@hotmail.it>
2017-11-23 14:26:39 +0100

    modified level map

--- app/src/main/java/com/polimi/dilapp/LevelmapActivity.java
+++ app/src/main/java/com/polimi/dilapp/LevelmapActivity.java
@@ -3,6 +3,8 @@ package com.polimi.dilapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class LevelmapActivity extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class LevelmapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_levelmap);
 
         ImageView carrotImage = (ImageView) findViewById(R.id.carrot);
+        Animation carrotAnimation = AnimationUtils.loadAnimation(this, R.anim.animation_definition);
+        carrotImage.startAnimation(carrotAnimation);
 
     }
 }
