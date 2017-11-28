e964f01a6c29f8b58c065c6de76f982bc8099fb2
marziadegiorgi <marziadegio@hotmail.it>
2017-11-28 11:05:49 +0100

    added button animation

--- app/src/main/java/com/polimi/dilapp/CreateAccountActivity.java
+++ app/src/main/java/com/polimi/dilapp/CreateAccountActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.polimi.dilapp.controller.LevelmapActivity;
+
 
 public class CreateAccountActivity extends AppCompatActivity{
 
--- app/src/main/java/com/polimi/dilapp/controller/LevelmapActivity.java
+++ app/src/main/java/com/polimi/dilapp/controller/LevelmapActivity.java
@@ -1,4 +1,4 @@
-package com.polimi.dilapp;
+package com.polimi.dilapp.controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -6,10 +6,12 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+
+import com.polimi.dilapp.R;
+import com.polimi.dilapp.model.BounceInterpolator;
 
 public class LevelmapActivity extends AppCompatActivity {
 
@@ -19,32 +21,45 @@ public class LevelmapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levelmap);
 
-        Button playButton = (Button) findViewById(R.id.playButton);
-
         startAnimation();
     }
 
+    /**
+     * Start the animations in the level map activity
+     */
    private void startAnimation() {
        ImageView carrotImage = (ImageView) findViewById(R.id.carrot);
        ImageView appleImage = (ImageView) findViewById(R.id.apple);
        ImageView pearImage = (ImageView) findViewById(R.id.pear);
+       Button playButton = (Button) findViewById(R.id.playButton);
 
-       Animation animationRight = AnimationUtils.loadAnimation(LevelmapActivity.this, R.anim.half_rotation_right);
-       Animation animationLeft = AnimationUtils.loadAnimation(LevelmapActivity.this, R.anim.half_rotation_left);
+       // Load animations
+       final Animation animationBounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+       final Animation animationRight = AnimationUtils.loadAnimation(LevelmapActivity.this, R.anim.half_rotation_right);
+       final Animation animationLeft = AnimationUtils.loadAnimation(LevelmapActivity.this, R.anim.half_rotation_left);
 
-       //set animation repeating to infinite
+       // Add the personilized interpolator for "animationBounce"
+       BounceInterpolator interpolator = new BounceInterpolator(0.7, 10);
+       animationBounce.setInterpolator(interpolator);
 
-
-       //start Images animation
+       // Start animations
+       playButton.startAnimation(animationBounce);
        carrotImage.startAnimation(animationRight);
        appleImage.startAnimation(animationRight);
        pearImage.startAnimation(animationLeft);
    }
 
+    /**
+     *  Display a popup menu when the menu button is clicked
+     * @param view
+     */
    public void showPopup( View view){
        PopupMenu popup = new PopupMenu(this, view);
        MenuInflater inflater = popup.getMenuInflater();
        inflater.inflate(R.menu.actions, popup.getMenu());
        popup.show();
    }
+
+   //TODO:Redirect to the right activity when Play Button is clicked
+
 }
