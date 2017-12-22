64ffa86eadccfbb0dcbad2974639c964d84617de
marziadegiorgi <marziadegio@hotmail.it>
2017-12-22 10:35:19 +0100

    fixed gradle crash

--- app/src/main/java/com/polimi/dilapp/levels/GamePresenter.java
+++ app/src/main/java/com/polimi/dilapp/levels/GamePresenter.java
@@ -21,7 +21,6 @@ import com.polimi.dilapp.database.DatabaseInitializer;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
