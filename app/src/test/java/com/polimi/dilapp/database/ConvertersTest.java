3711ffb84854d7b5ad41c9a3cb8bc2ca40bd1061
robertaiero <iero.roberta@gmail.com>
2018-05-11 17:51:58 +0200

    Added ConverterTest tests

--- app/src/test/java/com/polimi/dilapp/database/ConvertersTest.java
+++ app/src/test/java/com/polimi/dilapp/database/ConvertersTest.java
@@ -5,81 +5,67 @@ import android.arch.persistence.room.DatabaseConfiguration;
 import android.arch.persistence.room.InvalidationTracker;
 import android.arch.persistence.room.Room;
 
+import com.google.gson.Gson;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.when;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Room.class})
+@PrepareForTest({Gson.class})
 public class ConvertersTest {
 
     @Before
     public void test(){
-        PowerMockito.mockStatic(Room.class);
-        appDatabase = new AppDatabase() {
-            @Override
-            public ChildDao childDao() {
-                return null;
-            }
+        PowerMockito.mockStatic(Gson.class);
 
-            @Override
-            public ReportOneOneDao reportOneOneDao() {
-                return null;
-            }
-
-            @Override
-            public ReportOneTwoDao reportOneTwoDao() {
-                return null;
-            }
-
-            @Override
-            public ReportOneThreeDao reportOneThreeDao() {
-                return null;
-            }
-
-            @Override
-            public ReportTwoOneDao reportTwoOneDao() {
-                return null;
-            }
-
-            @Override
-            public ReportTwoTwoDao reportTwoTwoDao() {
-                return null;
-            }
-
-            @Override
-            public ReportTwoThreeDao reportTwoThreeDao() {
-                return null;
-            }
-
-            @Override
-            public ReportThreeOneDao reportThreeOneDao() {
-                return null;
-            }
-
-            @Override
-            public ReportThreeTwoDao reportThreeTwoDao() {
-                return null;
-            }
-
-            @Override
-            protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
-                return null;
-            }
-
-            @Override
-            protected InvalidationTracker createInvalidationTracker() {
-                return null;
-            }
-        };
     }
 
-    @Mock
-    private AppDatabase appDatabase;
 
-    
+    @Mock
+    private Converters converters;
+
+    @Mock
+    private String string;
+
+    @Mock
+    private Type type;
+
+    @Mock
+    Gson gson;
+
+    @Mock
+    private ArrayList<String> list;
+
+    //Here we test fromString()
+    @Test
+    public void fromStringTest(){
+        Converters.fromString(string);
+        PowerMockito.doReturn(gson.fromJson(string, type));
+    }
+
+    //Here we test fromArrayList()
+    @Test
+    public void fromArrayList(){
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("c");
+        arrayList.add("a");
+
+        when(gson.toJson(list)).thenReturn(string);
+        converters.fromArrayList(arrayList);
+        PowerMockito.doReturn(gson);
+    }
 }
